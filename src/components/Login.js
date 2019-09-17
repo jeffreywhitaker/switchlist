93485445a45495397919b4217c422d49268e0665
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-09-17 11:05:17 -0700

    added notes

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,46 +2,46 @@ import React, { useState } from 'react'
 import { axiosWithAuth } from '../utils/axiosAuth'
 
 function Login(props) {
- const [credentials, setCredentials] = useState({})
+    const [credentials, setCredentials] = useState({})
 
-const login = e => {
-    e.preventDefault()
-    axiosWithAuth().post('login/endpoint', credentials)
-        .then(res => {
-            localStorage.setItem('token', res.data.token)
-            props.history.push('/profile')
+    const login = e => {
+        e.preventDefault()
+        axiosWithAuth().post('login/endpoint', credentials)
+            .then(res => {
+                localStorage.setItem('token', res.data.token)
+                props.history.push('/profile')
+            })
+    }
+
+    const handleChange = e => {
+        setCredentials( {
+            ...credentials,
+            [e.target.name]: e.target.value,
         })
-}
+    }
 
-const handleChange = e => {
-    setCredentials( {
-        ...credentials,
-        [e.target.name]: e.target.value,
-    })
-}
-
-return (
-    <div>
-    <form onSubmit={login}>
-        <p>Username:</p>
-        <input
-            type="text"
-            name="username"
-            value={credentials.username}
-            onChange={handleChange}
-        />
-        <p>Password:</p>
-        <input
-            type="password"
-            name="password"
-            value={credentials.password}
-            onChange={handleChange}
-        />
-        <br/>
-        <br/>
-        <button>Log in</button>
-    </form>
-    </div>
+    return (
+        <div>
+        <form onSubmit={login}>
+            <p>Username:</p>
+            <input
+                type="text"
+                name="username"
+                value={credentials.username}
+                onChange={handleChange}
+            />
+            <p>Password:</p>
+            <input
+                type="password"
+                name="password"
+                value={credentials.password}
+                onChange={handleChange}
+            />
+            <br/>
+            <br/>
+            <button>Log in</button>
+        </form>
+        </div>
     )
 }
 
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { axiosWithAuth } from '../utils/axiosAuth'
 
 export default function UserProfile(props){
+    const [user, setUser] = useState() // need to set up useEffect hook on mount to get user data
+
+    const getData = () => {
+        axiosWithAuth().get('/data')
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
     return (
         <div>user profile test</div>
     )
