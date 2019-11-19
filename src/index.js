feaa9887277b23abb7911025766b289a090f428c
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-11-18 23:52:50 -0800

    add basic CRA style service worker

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter } from 'react-router-dom'
 
+import * as serviceWorker from './utils/serviceWorker';
+
 // import components and CSS
 import App from './App'
 import reducer from './reducers/rootReducer'
@@ -27,3 +29,8 @@ ReactDOM.render(
             <App />
         </BrowserRouter>
     </Provider>, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
