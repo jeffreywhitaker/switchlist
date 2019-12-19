8acaf30ab4066e9b80fa4e606f18c88e4d6d0a92
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-18 23:04:14 -0800

    add config for NODE ENV, restructure folders, turn of SW, configure for mongodb backend

--- public/switchlistServiceWorker.js
+++ public/switchlistServiceWorker.js
@@ -1,54 +1,51 @@
-const cacheName = 'v1';
+// const cacheName = "v1";
 
-const urlsToCache = [
-  '/'
-]
+// const urlsToCache = ["/"];
 
-// Call Install Event
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
-  e.waitUntil(
-    caches.open(cacheName)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache)
-      })
-  )
-});
+// // Call Install Event
+// window.addEventListener("install", e => {
+//   console.log("Service Worker: Installed");
+//   e.waitUntil(
+//     caches.open(cacheName).then(cache => {
+//       console.log("Opened Cache Storage");
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
 
-// Call Activate Event
-self.addEventListener('activate', (e) => {
-  console.log('Service Worker: Activated');
-  // Remove unwanted caches
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== cacheName) {
-            console.log('Service Worker: Clearing Old Cache');
-            return caches.delete(cache);
-          }
-        }),
-      );
-    }),
-  );
-});
+// // Call Activate Event
+// window.addEventListener("activate", e => {
+//   console.log("Service Worker: Activated");
+//   // Remove unwanted caches
+//   e.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         cacheNames.map(cache => {
+//           if (cache !== cacheName) {
+//             console.log("Service Worker: Clearing Old Cache");
+//             return caches.delete(cache);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
 
-// Call Fetch Event
-self.addEventListener('fetch', (e) => {
-  console.log('Service Worker: Fetching');
-  e.respondWith(
-    fetch(e.request)
-      .then((res) => {
-        // Make copy/clone of response
-        const resClone = res.clone();
-        // Open cahce
-        caches.open(cacheName).then((cache) => {
-          // Add response to cache
-          cache.put(e.request, resClone);
-        });
-        return res;
-      })
-      .catch((err) => caches.match(e.request).then((res) => res)),
-  );
-});
+// // Call Fetch Event
+// window.addEventListener("fetch", e => {
+//   console.log("Service Worker: Fetching");
+//   e.respondWith(
+//     fetch(e.request)
+//       .then(res => {
+//         // Make copy/clone of response
+//         const resClone = res.clone();
+//         // Open cahce
+//         caches.open(cacheName).then(cache => {
+//           // Add response to cache
+//           cache.put(e.request, resClone);
+//         });
+//         return res;
+//       })
+//       .catch(err => caches.match(e.request).then(res => res))
+//   );
+// });
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,105 +1,104 @@
 // import dependencies and functions
-import axios from 'axios'
-import { axiosWithAuth } from '../utils/axiosWithAuth'
+import axios from "axios";
+import { AXIOS_URI } from "../config/rootConfig";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 // sign up/register new user
-export const SIGNUP_USER_START = 'SIGNUP_USER_START'
-export const SIGNUP_USER_SUCCESS = 'SIGNUP_USER_SUCCESS'
-export const SIGNUP_USER_FAILURE = 'SIGNUP_USER_FAILURE'
-export const postUser = (credentials) => dispatch => {
-    dispatch({ type: SIGNUP_USER_START})
-    axios
-        .post('http://localhost:5000/users', credentials)
-        .then(res => {
-            console.log(res)
-            dispatch({ type: SIGNUP_USER_SUCCESS, payload: res.data})
-        })
-        .catch(err => {
-            console.log(`unable to register user: ${err}`)
-            dispatch({ type: SIGNUP_USER_FAILURE, payload: err})
-        })
-}
-
+export const SIGNUP_USER_START = "SIGNUP_USER_START";
+export const SIGNUP_USER_SUCCESS = "SIGNUP_USER_SUCCESS";
+export const SIGNUP_USER_FAILURE = "SIGNUP_USER_FAILURE";
+export const postUser = credentials => dispatch => {
+  dispatch({ type: SIGNUP_USER_START });
+  axios
+    .post(AXIOS_URI + "/users", credentials)
+    .then(res => {
+      console.log(res);
+      dispatch({ type: SIGNUP_USER_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(`unable to register user: ${err}`);
+      dispatch({ type: SIGNUP_USER_FAILURE, payload: err });
+    });
+};
 
 // login existing user
-export const LOGIN_USER_START = 'LOGIN_USER_START'
-export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
-export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE'
+export const LOGIN_USER_START = "LOGIN_USER_START";
+export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
+export const LOGIN_USER_FAILURE = "LOGIN_USER_FAILURE";
 export const userLogin = credentials => dispatch => {
-    dispatch({ type: LOGIN_USER_START})
-    axios
-        .post('http://localhost:5000/login', credentials)
-        .then(res => {
-            console.log(res)
-            localStorage.setItem("token", res.data.token)
-            dispatch({ type: LOGIN_USER_SUCCESS, payload: res.data})
-        })
-        .catch(err => {
-            console.log(`unable to load games data: ${err}`)
-            dispatch({ type: LOGIN_USER_FAILURE, payload: err})
-        })
-}
+  dispatch({ type: LOGIN_USER_START });
+  axios
+    .post(AXIOS_URI + "/login", credentials)
+    .then(res => {
+      console.log(res);
+      localStorage.setItem("token", res.data.token);
+      dispatch({ type: LOGIN_USER_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(`unable to load games data: ${err}`);
+      dispatch({ type: LOGIN_USER_FAILURE, payload: err });
+    });
+};
 
 // logout existing user
-export const LOGOUT_USER_START = 'LOGOUT_USER_START'
-export const LOGOUT_USER_SUCCESS = 'LOGOUT_USER_SUCCESS'
-export const LOGOUT_USER_FAILURE = 'LOGOUT_USER_FAILURE'
+export const LOGOUT_USER_START = "LOGOUT_USER_START";
+export const LOGOUT_USER_SUCCESS = "LOGOUT_USER_SUCCESS";
+export const LOGOUT_USER_FAILURE = "LOGOUT_USER_FAILURE";
 export const userLogout = () => dispatch => {
-    dispatch({ type: LOGOUT_USER_SUCCESS})
-    localStorage.setItem("token", null)
-}
-
+  dispatch({ type: LOGOUT_USER_SUCCESS });
+  localStorage.setItem("token", null);
+};
 
 // get list of games
-export const FETCHING_GAMES_START = 'FETCHING_GAMES_START'
-export const FETCHING_GAMES_SUCCESS = 'FETCHING_GAMES_SUCCESS'
-export const FETCHING_GAMES_FAILURE = 'FETCHING_GAMES_FAILURE'
+export const FETCHING_GAMES_START = "FETCHING_GAMES_START";
+export const FETCHING_GAMES_SUCCESS = "FETCHING_GAMES_SUCCESS";
+export const FETCHING_GAMES_FAILURE = "FETCHING_GAMES_FAILURE";
 export const getGames = () => dispatch => {
-    dispatch({ type: FETCHING_GAMES_START})
-    axios
-        .get('http://localhost:5000/api/gamelist')
-        .then(res => {
-            console.log(res)
-            dispatch({ type: FETCHING_GAMES_SUCCESS, payload: res.data})
-        })
-        .catch(err => {
-            console.log(`unable to load games data: ${err}`)
-            dispatch({ type: FETCHING_GAMES_FAILURE, payload: err})
-        })
-}
+  dispatch({ type: FETCHING_GAMES_START });
+  axios
+    .get(AXIOS_URI + "/games/all")
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCHING_GAMES_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(`unable to load games data: ${err}`);
+      dispatch({ type: FETCHING_GAMES_FAILURE, payload: err });
+    });
+};
 
 // get list of publishers
-export const FETCHING_PUBLISHERS_START = 'FETCHING_PUBLISHERS_START'
-export const FETCHING_PUBLISHERS_SUCCESS = 'FETCHING_PUBLISHERS_SUCCESS'
-export const FETCHING_PUBLISHERS_FAILURE = 'FETCHING_PUBLISHERS_FAILURE'
+export const FETCHING_PUBLISHERS_START = "FETCHING_PUBLISHERS_START";
+export const FETCHING_PUBLISHERS_SUCCESS = "FETCHING_PUBLISHERS_SUCCESS";
+export const FETCHING_PUBLISHERS_FAILURE = "FETCHING_PUBLISHERS_FAILURE";
 export const getPublishers = () => dispatch => {
-    dispatch({ type: FETCHING_PUBLISHERS_START})
-    axios
-        .get('http://localhost:5000/publishers')
-        .then(res => {
-            console.log(res)
-            dispatch({ type: FETCHING_PUBLISHERS_SUCCESS, payload: res.data})
-        })
-        .catch(err => {
-            console.log(`unable to load publishers data: ${err}`)
-            dispatch({ type: FETCHING_PUBLISHERS_FAILURE, payload: err})
-        })
-}
+  dispatch({ type: FETCHING_PUBLISHERS_START });
+  axios
+    .get(AXIOS_URI + "/publishers")
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCHING_PUBLISHERS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(`unable to load publishers data: ${err}`);
+      dispatch({ type: FETCHING_PUBLISHERS_FAILURE, payload: err });
+    });
+};
 
 // get user data
-export const FETCHING_USER_DATA_START = 'FETCHING_USER_DATA_START'
-export const FETCHING_USER_DATA_SUCCESS = 'FETCHING_USER_DATA_SUCCESS'
-export const FETCHING_USER_DATA_FAILURE = 'FETCHING_USER_DATA_FAILURE'
+export const FETCHING_USER_DATA_START = "FETCHING_USER_DATA_START";
+export const FETCHING_USER_DATA_SUCCESS = "FETCHING_USER_DATA_SUCCESS";
+export const FETCHING_USER_DATA_FAILURE = "FETCHING_USER_DATA_FAILURE";
 export const getUserData = () => dispatch => {
-    dispatch({ type: FETCHING_USER_DATA_START})
-    axiosWithAuth()
-        .get('  PLACEHOLDER!!!   ') // needs to be implemented in the back end
-        .then(res => {
-            console.log(res)
-            dispatch({ type: FETCHING_USER_DATA_SUCCESS, payload: res.data})
-        })
-        .catch(err => {
-            console.log(`unable to load user preferences data: ${err}`)
-            dispatch({ type: FETCHING_USER_DATA_FAILURE, payload: err})
-        })
-}
+  dispatch({ type: FETCHING_USER_DATA_START });
+  axiosWithAuth()
+    .get("  PLACEHOLDER!!!   ") // needs to be implemented in the back end
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCHING_USER_DATA_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(`unable to load user preferences data: ${err}`);
+      dispatch({ type: FETCHING_USER_DATA_FAILURE, payload: err });
+    });
+};
