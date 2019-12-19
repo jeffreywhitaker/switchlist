4bf276db0f5788b153097c933bd6e04b0fa1414d
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-18 23:18:07 -0800

    update rootConfig

--- src/config/rootConfig.js
+++ src/config/rootConfig.js
@@ -3,9 +3,9 @@ let AXIOS_URI;
 const NODE_ENV = process.env.REACT_APP_FORCE_NODE_ENV || process.env.NODE_ENV;
 
 if (NODE_ENV === "development") {
-  AXIOS_URI = "http://localhost:5000/";
+  AXIOS_URI = "http://localhost:5000";
 } else if (NODE_ENV === "production") {
-  AXIOS_URI = "http://switchlist.herokuapp.com/";
+  AXIOS_URI = "http://switchlist.herokuapp.com";
 }
 
 export { AXIOS_URI };
