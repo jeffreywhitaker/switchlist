391c497f137f080adcaf40cf9f781eac44dc85b9
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-19 20:53:43 -0800

    update root backend URI

--- src/config/rootConfig.js
+++ src/config/rootConfig.js
@@ -5,7 +5,7 @@ const NODE_ENV = process.env.REACT_APP_FORCE_NODE_ENV || process.env.NODE_ENV
 if (NODE_ENV === 'development') {
   AXIOS_URI = 'http://localhost:5000'
 } else if (NODE_ENV === 'production') {
-  AXIOS_URI = 'http://switchlist.herokuapp.com'
+  AXIOS_URI = 'http://switchlistsite-api.herokuapp.com'
 }
 
 export { AXIOS_URI }
